89d558bd8ef0069c98b19de74e0ae1fef7bc946e
pointtonull <point.to@gmail.com>
2018-03-06 15:35:24 +0000

    added example thingy

---
+++ thingies.py
@@ -0,0 +1,29 @@
+thingies = [
+
+    {
+        'title': 'MTB Specialized HardRock Sport Disc 29er Size L/19 +',
+        'short_description': 'bicycle',
+        'description': """
+            I am leaving the country and I can't take her with me.
+
+            This is my main bicycle.
+
+            I took her for maintenance every 3 months (to Bike Spanner).
+            Upgraded Handlebar and Mudguards because you will not stop
+            riding, not even with snow!
+
+            I am including starter biker equipment kit in the offer,
+            (that's a bargain!)
+
+             - High-Pressure Suspension Pump
+             - Two Spare new tubes
+             - Kryptonite U-Lock + 4ft Flex Cable
+             - Cleaning liquid
+             - Chain Lube
+             - Helmet
+        """,
+        'images': 'specialized*.jpg',
+        'price': 290,
+    },
+
+]
